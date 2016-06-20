f5e8e173d3cd2da6731009265ea88ad5252c125b
dongxu <i@huangdx.net>
2016-06-20 22:32:41 +0800

    backup

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -3,5 +3,20 @@
 var dashboardApp = angular.module('dashboardApp', []);
 
 dashboardApp.controller('LogEventController', function LogEventController($scope) {
-    $scope.events = [1,2,3]; 
+    $scope.init = function(wsHost) {
+        var ws = new WebSocket("ws://" + wsHost + "/ws");
+        ws.onopen = function(evt) {
+            console.log("OPEN");
+        }
+        ws.onclose = function(evt) {
+            console.log("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            console.log("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            console.log("ERROR: " + evt.data);
+        }
+    };
 });
